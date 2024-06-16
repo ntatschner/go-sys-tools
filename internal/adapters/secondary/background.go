1a992a0c45f29ca72ac4e44086c568241cdecdf6
Nigel Tatschner <ntatschner@gmail.com>
2024-06-16 09:34:17 +0100

    Updating primary port

--- internal/adapters/secondary/background.go
+++ internal/adapters/secondary/background.go
@@ -2,3 +2,15 @@ package adapters
 
 import "C"
 
+import (
+	"github.com/ntatschner/go-sys-tools/internal/core/domain"
+	"github.com/ntatschner/go-sys-tools/internal/core/ports/primary"
+)
+
+type BackgroundAdapter struct {
+	BackgroundPort primary.BackgroundPort
+}
+
+func (b BackgroundAdapter) GetBackground(path string) (domain.Background, error) {
+	return b.BackgroundPort.GetBackground(path)
+}
